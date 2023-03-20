54eeb5b8fc70bd9357666bc253ddba16005dbc30
jjin0ee <jjin0ee@littlefox.com>
2023-03-20 14:08:56 +0900

    state 사용해보기

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ import Content from "./components/Content";
 import Subject from "./components/Subject";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        Subject: {title: 'WEB', sub: 'World Wide Web!'},
+        contents: [
+          {id:1, title:'HTML', desc:'HTML is HyperText ...'},
+          {id:2, title:'CSS', desc:'CSS is for design'},
+          {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+        ]
+    }
+}
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
+        <Subject title={this.state.Subject.title} sub={this.state.Subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperTest markup Language."></Content>
       </div>
     );
